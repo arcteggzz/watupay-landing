a79363ac6585613241386822cf21478777b956dc
Oghenetega Esedere <arcteggzz@gmail.com>
2023-10-11 23:17:29 +0100

    feat: implemented navbar

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a mini application created for the Watupay-Frontend test interview. The web application aims to mock a simple landing page with a hero section, navigation, carousel, footer and an Active Contests section.
 
-## HOW TO TEST
+## HOW TO RUN ON LOCAL ENVIRONMENT
 
 Step 1: Clone the repository from github.com.
 
@@ -16,7 +16,7 @@ Step 2: cd into the root directory.
 cd watupay-landing
 ```
 
-step 3: install dependencies by running npm i.
+step 3: Install dependencies by running npm install.
 
 ```bash
 npm install
@@ -30,7 +30,14 @@ npm run dev
 
 step 5: Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-## Packages Used
+## HOW TO TEST ON LIVE SERVER
+
+If running on your local server is tasking or time consuming, another option to test on a live server hosted on Netlify and Vercel exists. You can visit any of the available links by clicking the links below.
+
+1. [Watupay Landing Page on Vercel](https://watupay-landing.vercel.app)
+2. [Watupay Landing Page on Netlify](https://watupay-landing.netlify.app)
+
+## TECHNOLOGIES UTILIZED
 
 1. Next13
 2. Typescript
--- src/components/layoutComponents/Navbar/index.tsx
+++ src/components/layoutComponents/Navbar/index.tsx
@@ -1,11 +1,97 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import watu_vote_logo from "./images/watu_vote_logo.png";
+import closeBurger from "./images/closeBurger.png";
+import openBurger from "./images/openBurger.png";
+import { LinkWIthIcon } from "@/components/reuseables";
 import { routePaths } from "@/utils";
+import { useState } from "react";
 
 const Navbar = () => {
+  const navElements = [
+    { link: routePaths.HOMEPAGE, name: "Home" },
+    { link: "/", name: "Contests" },
+    { link: "/", name: "Pricing & FAQs" },
+  ];
+
+  const pathname = usePathname();
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  console.log(pathname);
+
   return (
     <>
-      <nav className={styles.Navbar}>Navbar</nav>
+      <nav className={styles.Navbar}>
+        <div className={styles.desktop_Nav}>
+          <Link href={"/"} className={styles.logo_icon}>
+            <img src={watu_vote_logo.src} alt="Watuvote icon" />
+          </Link>
+
+          {/* desktop middle */}
+          <div className={styles.navLinks_Desktop}>
+            {navElements.map((elem) => {
+              return (
+                <Link
+                  href={elem.link}
+                  key={elem.name}
+                  className={
+                    pathname === elem.link
+                      ? styles.active_NavLink
+                      : styles.inactive_NavLink
+                  }
+                >
+                  {elem.name}
+                </Link>
+              );
+            })}
+          </div>
+
+          <div className={styles.link_container}>
+            <LinkWIthIcon linkText="Create Contest" variant="light" />
+          </div>
+
+          {/* mobile hamburger controller */}
+          <button
+            onClick={() => setMobileNavOpen((mobileNavOpen) => !mobileNavOpen)}
+            className={styles.hamburger}
+          >
+            <img
+              src={mobileNavOpen ? closeBurger.src : openBurger.src}
+              alt="Hamburger Menu Button"
+            />
+          </button>
+        </div>
+
+        <div
+          className={
+            mobileNavOpen
+              ? styles.Mobile_NavbarOpen
+              : styles.Mobile_NavbarClosed
+          }
+        >
+          <section className={styles.Navlinks_Mobile}>
+            {navElements.map((elem) => {
+              return (
+                <Link
+                  href={elem.link}
+                  key={`${elem.link}_mobile`}
+                  className={
+                    pathname === elem.link
+                      ? styles.active_mobile_NavLink
+                      : styles.inactive_mobile_NavLink
+                  }
+                >
+                  {elem.name}
+                </Link>
+              );
+            })}
+          </section>
+          <LinkWIthIcon linkText="Create Contest" variant="light" />
+        </div>
+      </nav>
     </>
   );
 };
